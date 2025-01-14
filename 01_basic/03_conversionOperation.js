ae7a8dc86f5544270f0ab96ad81812dfe2e342ec
Sidd0710 <130654532+Sidd-0710@users.noreply.github.com>
2025-01-14 14:13:02 +0000

    adding operations stage

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -25,10 +25,48 @@
   // if u wirte another than 0 & 1 =>  true
 
 
-  let someNumber = 66
-  let stringNumber = String(someNumber)
-   console.log(stringNumber);
-   console.log( typeof (stringNumber));
+//   let someNumber = 66
+//   let stringNumber = String(someNumber)
+//    console.log(stringNumber);
+//    console.log( typeof (stringNumber));
 
-   
-   
+
+   // ----------------Operations---------------//
+
+// let value = 44
+// let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+7);
+// console.log(2-7);
+// console.log(2*7);
+// console.log(2**9);
+// console.log(2**9);
+// console.log(2**9);
+// console.log(2**9);
+// console.log(2**9);
+
+
+// let str1 = "Hello"
+// let str2 = "Sidd"
+ 
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(2 + "2");
+// console.log(1 + 2 + "3" );
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1 ,num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter= 100
+gameCounter++;
+
+console.log(gameCounter);
