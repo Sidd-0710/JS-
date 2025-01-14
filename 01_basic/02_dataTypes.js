bff9c642a334004911bef556af5f5ca3a561b7bd
Sidd0710 <130654532+Sidd-0710@users.noreply.github.com>
2025-01-14 12:22:26 +0000

    Learning about Datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using npde js ,not web browser 
+
+let name =" sidd"
+let age= 20
+let isLoggedIn = false
+let state = null
+let current ;
+
+
+//bigint
+// string--> ""  
+//boolean => true/false
+//null -=> standalone value
+//undefined => 
+// symbol => unique
+
+
+//object 
+
+
+console.log(typeof" Sidd");
+
+console.log( typeof undefined); //output is undefined
+
+console.log(typeof null );//output is object
