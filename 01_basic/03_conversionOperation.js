6de5d11741ba81cdd7b8c3c00cab6dfa68d48d7b
Sidd0710 <130654532+Sidd-0710@users.noreply.github.com>
2025-01-14 13:27:04 +0000

    Conversion  of Datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = "sidd"
+ 
+
+//  console.log(typeof score);
+// //  console.log(typeof (score)); // another way to write console log
+
+//  let valueInNumber = Number(score)
+
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+ 
+ // "33" => 33
+ // "33acb" => Nan
+ // true -> 1, false => 0 
+   
+//  let isLoggedIn = 445
+
+//   let booleanIsLoggedIN = Boolean(isLoggedIn)
+//   console.log(booleanIsLoggedIN );
+  
+
+  // 1 => true; 0 => false
+  // "" => false
+  //"sidd" => true
+  // if u wirte another than 0 & 1 =>  true
+
+
+  let someNumber = 66
+  let stringNumber = String(someNumber)
+   console.log(stringNumber);
+   console.log( typeof (stringNumber));
+
+   
+   
