27ff623acee533a706822355ac5a24f167b17b23
Siddheshwar Vilas Chirkute <130654532+Sidd-0710@users.noreply.github.com>
2025-01-16 14:58:46 +0000

    learning Objects

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,16 @@
+// singleton   
+//  Object.create
+
+// object literals
+const jsUser = {
+    name :" Sidd",
+    age : 18,
+    location : "Pen",
+    email : "sidd@gmail.com",
+    isLoggedIN : false,
+    LastLoggedIn : ["Monday", "FridAY"]
+}
+
+console.log(jsUser.email );
+console.log(jsUser.email );
+
