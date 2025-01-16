9cd238faa2b6ab550d6c3f5c559373134fb5e86d
Siddheshwar Vilas Chirkute <130654532+Sidd-0710@users.noreply.github.com>
2025-01-16 15:14:37 +0000

    Editing the file

--- 02_basic/03_objects.js
+++ 02_basic/03_objects.js
@@ -1,16 +1,38 @@
-// singleton   
-//  Object.create
+// singleton
+// Object.create
 
 // object literals
-const jsUser = {
-    name :" Sidd",
-    age : 18,
-    location : "Pen",
-    email : "sidd@gmail.com",
-    isLoggedIN : false,
-    LastLoggedIn : ["Monday", "FridAY"]
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(jsUser.email );
-console.log(jsUser.email );
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
 
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
