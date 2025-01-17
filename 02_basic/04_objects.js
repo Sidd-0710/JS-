9128d6ac1c2f35d1945f127da04c67a63185b403
Siddheshwar Vilas Chirkute <130654532+Sidd-0710@users.noreply.github.com>
2025-01-17 14:33:40 +0000

    Objects 2 part

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,55 @@
+// const  tinder =new Object() //these is a singleton object 
+
+const tinder ={} // thses is not a singleton object 
+
+ tinder.id ="123"
+ tinder.name="sidd"
+ tinder.isLoggedIn= false
+
+// console.log(tinder);
+
+const regularUser ={
+    email : "sidd@gamil",
+    fullname :{
+        UserFullName : {
+            firstname : "sodd",
+            lastname :"Djsnb,"
+        }
+    }
+}
+// console.log(regularUser.fullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+
+console.log(tinder.hasOwnProperty('isLoggedIn'));
