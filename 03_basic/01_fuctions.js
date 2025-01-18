12a660dede1197d8ba9b0dfaa2f291cb52e10e21
Siddheshwar Vilas Chirkute <130654532+Sidd-0710@users.noreply.github.com>
2025-01-18 07:47:50 +0000

    Functions part 1

---
+++ 03_basic/01_fuctions.js
@@ -0,0 +1,37 @@
+ 
+function sayMyname(){
+    console.log("s");
+    console.log("i");
+    console.log("d");
+    console.log("d");
+    
+}
+
+//  
+
+function addTwoNumbers(number1, number2 ){
+
+   let result = number1 + number2
+    return result 
+
+return number1 + number2
+       
+} 
+
+ const result = addTwoNumbers(90,10)
+
+//  console.log("Result : ",result);
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("Plzz Enter a Username");
+        return 
+      
+    }
+
+    return `${username} just Logged in`
+
+}
+  
+// console.log(loginUserMessage("sidd"));
+console.log(loginUserMessage("sidd"));
