1cca838dd636b15a07590a34e56c822dc2682ffe
Siddheshwar Vilas Chirkute <130654532+Sidd-0710@users.noreply.github.com>
2025-01-18 14:27:05 +0000

    Fuctions Part2

--- 03_basic/01_fuctions.js
+++ 03_basic/01_fuctions.js
@@ -34,4 +34,37 @@ function loginUserMessage(username = "sam"){
 }
   
 // console.log(loginUserMessage("sidd"));
-console.log(loginUserMessage("sidd"));
+// console.log(loginUserMessage("sidd"));
+
+function  CartPrice(...num1){
+    return num1
+}
+
+// console.log(CartPrice(200, 4000,800 ,500));
+
+const user = {
+    username : "sidd",
+    prices : 199
+}
+ 
+function handleObject(anyobject){
+ 
+    console.log(`username is = ${anyobject.username} and price is = ${anyobject.price}`);
+
+    
+}
+
+// handleObject(user)
+handleObject({
+    username : "baaa",
+    price : 700,
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
