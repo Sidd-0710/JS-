6d801cb020cbd3516b4c9a851c7db186305a1176
Siddheshwar Vilas Chirkute <130654532+Sidd-0710@users.noreply.github.com>
2025-01-20 14:01:49 +0000

    Editing the File

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -1,59 +1,62 @@
-//var c = 300
-let a = 300
-if (true) {
-    let a = 10
-    const b = 20
-    // console.log("INNER: ", a);
-    
-}
+//  var c =3000
+
+//  let a = 200 
+
+// if (true) {
+//     let a = 10
+// const b  = 20
+// var c = 30 
+// // console.log("Inner :- ",a);
+
+// }
+
 
 // console.log(a);
 // console.log(b);
 // console.log(c);
 
 
-function one(){
-    const username = "hitesh"
+// function one(){
+//     const username = "sidd"
 
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-    // console.log(website);
+//     function two (){
+//         const websites ="youtube"
+//         console.log(username);
+        
+//     }
+//     // console.log(websites);
 
-     two()
+//     two()
 
-}
+
+    
+// }
 
 // one()
 
-if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
-        const website = " youtube"
-        // console.log(username + website);
-    }
-    // console.log(website);
-}
+
+// if (true) {
+//     const username = "sidd"
+//      if (username === "sidd") {
+//         const website =" hello "
+//         console.log(username + website);
+    
+//      }
+//     //  console.log(website);
+     
+// }
 
 // console.log(username);
 
-
-// ++++++++++++++++++ interesting ++++++++++++++++++
-
-
-console.log(addone(5))
+// console.log(addone(5))
 
 function addone(num){
-    return num + 1
+    return num +1 
+
 }
 
-
-
-
-
-const addTwo = function(num){
+const addtwo = function(num){
     return num + 2
 }
 
-addTwo(5)
+addtwo(6)
