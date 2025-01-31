8f5f7c18089b83bcc893da59d836506792788e6f
Siddheshwar Vilas Chirkute <130654532+Sidd-0710@users.noreply.github.com>
2025-01-31 07:28:35 +0000

    inner text $ iuter text

---
+++ 06_dom/script.js
@@ -0,0 +1,13 @@
+//inner text & outer text
+
+
+const a = document.querySelector('h1').innerText;
+console.log(a)
+
+console.log("----------");
+
+const b = document.querySelector('h1').textContent;
+console.log(b)
+
+
+
