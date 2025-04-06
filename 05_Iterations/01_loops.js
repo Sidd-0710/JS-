8459080da59b25e34abbaa94704a536ca049c563
Siddheshwar Vilas Chirkute <130654532+Sidd-0710@users.noreply.github.com>
2025-04-06 10:18:49 +0000

    Error Solved In Loops

--- 05_Iterations/01_loops.js
+++ 05_Iterations/01_loops.js
@@ -30,14 +30,14 @@ for (let i = 1; i <= 10; i++) {
 
 // break and continue
 
-// for (let index = 1; index <= 20; index++) {
-//     if (index == 5) {
-//         console.log(`Detected 5`);
-//         break
-//     }
-//    console.log(`Value of i is ${index}`);
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        break
+    }
+   console.log(`Value of i is ${index}`);
     
-// }
+}
 
 for (let index = 1; index <= 20; index++) {
     if (index == 5) {
