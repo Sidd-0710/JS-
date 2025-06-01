5aec778cc90a31b23f76735f3c403402cf22cfd0
Siddheshwar Vilas Chirkute <130654532+Sidd-0710@users.noreply.github.com>
2025-06-01 07:29:14 +0000

    New String Content Add

--- 01_basic/06_strings.js
+++ 01_basic/06_strings.js
@@ -10,10 +10,10 @@
 
 
 
-const name = "sidd"
-const repoCount = 5
+// const name = "sidd"
+// const repoCount = 5
 
-// console.log(name + repoCount + " Value");
+// // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
@@ -46,3 +46,26 @@ console.log(url.includes('sundar'))
 
 console.log(gameName.split('-'));
 
+
+
+let name = "Harry"
+// console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+let friend = 'Prakash'
+console.log(friend)
+// let myfriend = "Shubh' // Dont do this
+// console.log(myfriend)
+
+// Template literals
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+// Nikhil is a friend of Pramod
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+// Escape Sequence Characters
+// let fruit = 'Bana\'na'
+// console.log(fruit)
+let fruit = "Bana\"na"
+console.log(fruit)
