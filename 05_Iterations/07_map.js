7c2d555618ffa49f67c5cc245c373ff4b1f19273
Siddheshwar Vilas Chirkute <130654532+Sidd-0710@users.noreply.github.com>
2025-06-02 18:25:06 +0000

    Added Content On Array Map Method

--- 05_Iterations/07_map.js
+++ 05_Iterations/07_map.js
@@ -1,10 +1,18 @@
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const newNums = myNumers.map( (num) => { return num + 10})
+// const newNums = myNumers.map( (num) => { return num + 10})
 
-// const newNums = myNumers
-//                 .map((num) => num * 10 )
-//                 .map( (num) => num + 1)
-//                 .filter( (num) => num >= 40)
+// // const newNums = myNumers
+// //                 .map((num) => num * 10 )
+// //                 .map( (num) => num + 1)
+// //                 .filter( (num) => num >= 40)
 
-console.log(newNums);
+// console.log(newNums);
+
+
+// Array map method
+let a = myNumers.map((value, index, array) => {
+  // console.log(value, index, array)
+  return value + index
+})
+console.log(a, myNumers)
